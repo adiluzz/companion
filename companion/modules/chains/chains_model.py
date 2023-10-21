845b9a9e88c54ba87c9bb9148f68a7d5c4e6ad41
adiluzz <adiluzz@gmail.com>
2023-10-21 19:45:53 +0300

    Changed chains model

--- companion/modules/chains/chains_model.py
+++ companion/modules/chains/chains_model.py
@@ -3,9 +3,14 @@ from mongoengine import *
 
 connect('comapnion')
 
+class ChainError(Document):
+    error_time = DateTimeField()
+    error_texr = StringField()
 
 class Chain(DynamicDocument):
     title = StringField(required=True, max_length=200)
     created = DateTimeField(default=datetime.datetime.utcnow)
+    finished = DateTimeField()
+    error = ChainError()
     chain = StringField()
     meta = {'allow_inheritance': True}
