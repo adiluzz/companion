f42db6575c59625d0ae7a73637f12df066f1c2f1
adiluzz <adiluzz@gmail.com>
2023-12-06 17:46:32 +0200

    Removed beaarer

--- companion/utils/auth.py
+++ companion/utils/auth.py
@@ -63,7 +63,8 @@ class CognitoAuthenticator:
         Returns:
             True if valid, False otherwise
         """
-
+        if token.startswith("Bearer "):
+            token = token.removeprefix("Bearer ")
         try:
             self._is_jwt(token)
             self._get_verified_header(token)
@@ -218,6 +219,7 @@ class CognitAuthMiddleware(object):
             client_id=COGNITO_AUDIENCE,
         )
         access_token = request.headers.get('Authorization')
+        print(access_token)
         token_verified = auth.verify_token(access_token)
         if token_verified == False:
             return self.process_exception(request=request, exception='Not Authorized')
