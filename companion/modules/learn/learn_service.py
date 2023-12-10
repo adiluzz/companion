4018d997769daa864900bcda6bf8f138fd3a7747
MrKelada1 <danieler.main@gmail.com>
2023-12-10 15:07:11 +0200

    Trying to use RAG: Loaded text from blog using code from https://python.langchain.com/docs/use_cases/question_answering/#retrieval-and-generation

--- companion/modules/learn/learn_service.py
+++ companion/modules/learn/learn_service.py
@@ -1,9 +1,14 @@
-from huggingface_hub import notebook_login
-from datasets import load_dataset
-class LearnService:    
-    def learn_from_file():
-        load_dataset
-        # notebook_login()
-        
+from langchain.document_loaders import WebBaseLoader
 
-
+def learn_from_file():
+    loader = WebBaseLoader(
+        web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+        bs_kwargs={
+            "parse_only": bs4.SoupStrainer(
+                class_=("post-content", "post-title", "post-header")
+            )
+        },
+    )
+    docs = loader.load()
+    len(docs[0].page_content)
+    print(docs[0].page_content[:500])
