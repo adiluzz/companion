b809230188bd4fcdc021b19c2025cd2d7650f4a2
MrKelada1 <danieler.main@gmail.com>
2023-12-10 15:15:34 +0200

    Fixed import error

--- companion/modules/learn/learn_service.py
+++ companion/modules/learn/learn_service.py
@@ -1,14 +1,16 @@
 from langchain.document_loaders import WebBaseLoader
 
-def learn_from_file():
-    loader = WebBaseLoader(
-        web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-        bs_kwargs={
-            "parse_only": bs4.SoupStrainer(
-                class_=("post-content", "post-title", "post-header")
-            )
-        },
-    )
-    docs = loader.load()
-    len(docs[0].page_content)
-    print(docs[0].page_content[:500])
+class LearnService:
+    
+    def learn_from_file():
+        loader = WebBaseLoader(
+            web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+            bs_kwargs={
+                "parse_only": bs4.SoupStrainer(
+                    class_=("post-content", "post-title", "post-header")
+                )
+            },
+        )
+        docs = loader.load()
+        len(docs[0].page_content)
+        print(docs[0].page_content[:500])
