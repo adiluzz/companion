751ab06fedcc1501351101cc5b58d73669a13933
MrKelada1 <danieler.main@gmail.com>
2023-12-10 15:19:04 +0200

    added imports

--- companion/modules/learn/learn_service.py
+++ companion/modules/learn/learn_service.py
@@ -1,7 +1,14 @@
 from langchain.document_loaders import WebBaseLoader
-
+import bs4
+from langchain import hub
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
 class LearnService:
-    
+
     def learn_from_file():
         loader = WebBaseLoader(
             web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
