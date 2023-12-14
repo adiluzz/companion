ad67a0d724d80b4bab93d6297abea3cea9c8f925
adiluzz <adiluzz@gmail.com>
2023-12-14 12:54:16 +0200

    Added delete chain to API

--- companion/chain.py
+++ companion/chain.py
@@ -1,3 +1,4 @@
+import copy
 from django.http import HttpResponse
 import json
 from companion.modules.chains.chains_model import Chain
@@ -7,11 +8,18 @@ from companion.modules.chains.chains_service import ChainsService
 
 def index(request, chain_id):
     if request.method == 'GET':
-        print("chain_id " + chain_id)
         chain = Chain.objects(pk=chain_id)
-        print(chain)
         response_data = {}
         response_data['chain'] = json.loads(chain[0].to_json())
         return HttpResponse(json.dumps(response_data), content_type="application/json")
+    
+    if request.method == 'DELETE':
+        chain = Chain.objects(pk=chain_id)
+        chain_to_return = copy.deepcopy(chain[0])
+        chain.delete()
+        response_data = {}
+        response_data['chain'] = json.loads(chain_to_return.to_json())
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
+	
 
     return HttpResponse("Hello, world. You're at the chains index.")
