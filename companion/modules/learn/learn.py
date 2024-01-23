5e9e174c4ff118119211c7e0838bd11f02818bb3
adiluzz <adiluzz@gmail.com>
2024-01-23 13:49:03 +0200

    Cleanup

--- companion/modules/learn/learn.py
+++ companion/modules/learn/learn.py
@@ -1,14 +1,10 @@
 from django.http import HttpResponse
-import json
-from companion.modules.chains.chains_model import Chain
-from companion.modules.chains.chains_service import ChainsService
 from companion.modules.learn.learn_service import LearnService
 
 
-
 def index(request):
-	if request.method == 'POST':
-		response = LearnService.run_qa_chain()
-		return HttpResponse('Success', content_type="application/json")
-	
-	return HttpResponse("Hello, world. You're at the learn index.")
+    if request.method == 'POST':
+        response = LearnService.run_qa_chain()
+        return HttpResponse('Success', content_type="application/json")
+
+    return HttpResponse("Hello, world. You're at the learn index.")
