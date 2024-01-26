636950890e1ef0c553ccff707060b69f5b6cb5fc
adiluzz <adiluzz@gmail.com>
2024-01-26 15:49:16 +0200

    Added createa database function

--- companion/modules/databases/databases_service.py
+++ companion/modules/databases/databases_service.py
@@ -1,6 +1,43 @@
 
 
 from companion.modules.databases.database_model import Database
+import os
+import multiprocessing
+from langchain.embeddings import LlamaCppEmbeddings
+import os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+DB_FAISS_PATH = 'vectorstore/db_faiss'
+DATA_PATH = 'demodata/'
+n_ctx = 4096
+bucket_name = 'companion-databases'
+
+# def get_data_path(id):
+    
+
+def get_llama_embeddings():
+    embedding = LlamaCppEmbeddings(
+        model_path=os.environ['MODEL_PATH'],
+        n_threads=max(multiprocessing.cpu_count() - 1, 1),
+        n_ctx=n_ctx)
+    return embedding
+
+def create_vector_db(db):
+    loader = DirectoryLoader(DATA_PATH,
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,
+                                                   chunk_overlap=50)
+    texts = text_splitter.split_documents(documents)
+    llama_embeddings = get_llama_embeddings()
+    db = FAISS.from_documents(texts, llama_embeddings)
+    db.save_local(DB_FAISS_PATH)
+    return db
 
 
 def create_database(document_ids, name):
